8bb9340e7caeaa237d076e7f7297d3bf2e5a15d7
Andrew Fagerheim <amf2288@swotservd.ldeo.columbia.edu>
2022-06-30 12:05:53 -0400

    added temp and sal outputs to float_load

--- my_floats.py
+++ my_floats.py
@@ -74,4 +74,7 @@ def float_load(float_id):
     levels = list(range(0, 1500))
     afloat_interp = afloat_prof.argo.interp_std_levels(levels)
     
-    return afloat_interp
+    TEMP_afloat = afloat.TEMP
+    SAL_afloat = afloat.PSAL
+    
+    return afloat_interp, TEMP_afloat, SAL_afloat
