d208ce71a0b49e4ece0f56010de992d3ea73ee8c
Andrew Fagerheim <amf2288@swotservd.ldeo.columbia.edu>
2023-04-21 11:29:30 -0400

    added functions for NEV and drho/dz

--- EV_funcs.py
+++ EV_funcs.py
@@ -8,7 +8,7 @@ import matplotlib
 
 import filt_funcs as ff
 
-def get_mean_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED'):
+def get_mean_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', bound=False):
     
     '''Takes an xarray and a filter scale in meters and returns an array of the filtered signal, squared.
     
@@ -18,13 +18,13 @@ def get_mean_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTE
     dim1: profile dimension, default=N_PROF
     dim2: pressure dimension, default=PRES_INTERPOLATED'''
     
-    ds_filt = ff.get_filt_single(ds, lfilter, variable=variable, dim1=dim1, dim2=dim2)
+    ds_filt = ff.get_filt_single(ds, lfilter, variable=variable, dim1=dim1, dim2=dim2, bound=bound)
     ds_mean_variance = ds_filt**2
     
     return ds_mean_variance
 
 
-def get_total_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED'):
+def get_total_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', bound=False):
     
     '''Takes an xarray and a filter scale in meters and returns an array of the squared signal, filtered.
     
@@ -35,12 +35,12 @@ def get_total_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INT
     dim2: pressure dimension, default=PRES_INTERPOLATED'''
     
     ds_squared = ds[[variable]]**2
-    ds_total_variance = ff.get_filt_single(ds_squared, lfilter, variable=variable, dim1=dim1, dim2=dim2)
+    ds_total_variance = ff.get_filt_single(ds_squared, lfilter, variable=variable, dim1=dim1, dim2=dim2, bound=bound)
     
     return ds_total_variance
 
 
-def get_eddy_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED'):
+def get_eddy_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', bound=False):
     
     '''Takes an xarray and a filter scale in meters and returns an array of the difference between the signal squared then filtered, and the signal filtered then squared. (This quantity we're currently defining as 'EKE')
     
@@ -50,8 +50,23 @@ def get_eddy_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTE
     dim1: profile dimension, default=N_PROF
     dim2: pressure dimension, default=PRES_INTERPOLATED'''
     
-    ds_mean_variance = get_mean_variance(ds, lfilter, variable=variable, dim1=dim1, dim2=dim2)
-    ds_total_variance = get_total_variance(ds, lfilter, variable=variable, dim1=dim1, dim2=dim2)
+    ds_mean_variance = get_mean_variance(ds, lfilter, variable=variable, dim1=dim1, dim2=dim2, bound=bound)
+    ds_total_variance = get_total_variance(ds, lfilter, variable=variable, dim1=dim1, dim2=dim2, bound=bound)
     ds_eddy_variance = ds_total_variance - ds_mean_variance
     
     return ds_eddy_variance
+
+
+def get_NEV(ds,ds_EV,variable,coarsen_scale=40):
+    
+    return ((ds_EV.mean('N_PROF')/ get_drho_dz(ds, variable=variable, coarsen_scale=coarsen_scale).mean('N_PROF')**2)**(1/2))
+
+
+def get_drho_dz (ds, variable, coarsen_scale=5):
+    coarsened_rho = ds[variable].coarsen(PRES_INTERPOLATED=coarsen_scale).mean()
+    
+    drho_dz_coarsened = coarsened_rho.diff('PRES_INTERPOLATED')/(2*coarsen_scale)
+    
+    drho_dz = drho_dz_coarsened.interp(PRES_INTERPOLATED=ds.PRES_INTERPOLATED)
+    
+    return drho_dz
