056adf9d2b367c9940b1a940d615269479ae45ac
Andrew Fagerheim <amf2288@swotservd.ldeo.columbia.edu>
2023-05-18 14:32:03 -0400

    depth to density panel plotting function

--- density_funcs.py
+++ density_funcs.py
@@ -1,2 +1,69 @@
 #DEPTH TO DENSITY FUNCTIONS
 
+import xarray as xr
+import matplotlib.pyplot as plt
+import numpy as np
+import argopy
+import scipy.ndimage as filter
+import scipy
+import matplotlib
+import gsw
+from cmocean import cm as cmo
+import scipy.interpolate as interpolate
+
+import filt_funcs as ff
+import EV_funcs as ef
+import plot_funcs as pf
+
+
+def func_var_int(ds, var, rho_grid, dim1='N_PROF_NEW', dim2='PRES_INTERPOLATED', flag='group'):
+    
+    N_PROF_num = ds[dim1].values
+    
+    rho = ds.SIG0
+    rho_nonan = rho.where(~np.isnan(rho), drop=True)
+    
+    var_nonan = ds[var].where(~np.isnan(rho), drop=True)
+    
+    var_nonan2 = var_nonan.where(~np.isnan(var_nonan), drop=True)
+    
+    if flag == 'group': # incase density is identical b/w two points (this makes things very slow)
+        var_nonan = var_nonan.groupby(rho_nonan).mean()
+        rho_nonan = rho_nonan.groupby(rho_nonan).mean()
+    
+    if (len(rho_nonan)>2) & (len(var_nonan2)>2):
+        fvar = interpolate.PchipInterpolator(rho_nonan, var_nonan, extrapolate=False)
+    
+        var_tilde = fvar(rho_grid)
+    else:
+        var_tilde = np.nan*rho_grid
+    
+    return xr.DataArray(var_tilde.reshape((-1,1)),
+                        dims = ['rho_grid', dim1],
+                        coords = {'rho_grid': rho_grid, dim1: [N_PROF_num]}).rename(var)
+
+
+
+def interpolate2density(ds_z, rho_grid, dim1='N_PROF_NEW', dim2='PRES_INTERPOLATED'):
+    
+    N_PROF_ind = 0
+    pres_tilde_xr  = func_var_int(ds_z.isel(N_PROF=N_PROF_ind), dim2,rho_grid)
+    CT_tilde_xr    = func_var_int(ds_z.isel(N_PROF=N_PROF_ind), 'CT',rho_grid)
+    SA_tilde_xr    = func_var_int(ds_z.isel(N_PROF=N_PROF_ind), 'SA', rho_grid)
+    SIG0_tilde_xr  = func_var_int(ds_z.isel(N_PROF=N_PROF_ind), 'SIG0', rho_grid)
+    SPICE_tilde_xr = func_var_int(ds_z.isel(N_PROF=N_PROF_ind), 'SPICE', rho_grid)
+
+    for N_PROF_ind in range(1, len(ds_z.N_PROF)):
+        if np.mod(N_PROF_ind, 50)==0:
+            print(N_PROF_ind)
+        pres_tilde_xr  = xr.concat([pres_tilde_xr , func_var_int(ds_z.isel(N_PROF=N_PROF_ind), dim2, rho_grid)], dim=dim1)
+        CT_tilde_xr    = xr.concat([CT_tilde_xr , func_var_int(ds_z.isel(N_PROF=N_PROF_ind), 'CT', rho_grid)], dim=dim1)
+        SA_tilde_xr    = xr.concat([SA_tilde_xr , func_var_int(ds_z.isel(N_PROF=N_PROF_ind), 'SA', rho_grid)], dim=dim1)
+        SIG0_tilde_xr  = xr.concat([SIG0_tilde_xr , func_var_int(ds_z.isel(N_PROF=N_PROF_ind), 'SIG0', rho_grid)], dim=dim1)
+        SPICE_tilde_xr = xr.concat([SPICE_tilde_xr , func_var_int(ds_z.isel(N_PROF=N_PROF_ind), 'SPICE', rho_grid)], dim=dim1)
+    
+
+    ds_rho = xr.merge([pres_tilde_xr, CT_tilde_xr,
+                             SA_tilde_xr, SIG0_tilde_xr, SPICE_tilde_xr])
+    
+    return ds_rho
