ca14278cb31b0521532723a358775528dc5d60f8
Andrew Fagerheim <amf2288@swotservd.ldeo.columbia.edu>
2023-08-24 12:57:17 -0400

    added EKE function

--- EV_funcs.py
+++ EV_funcs.py
@@ -10,7 +10,23 @@ import matplotlib
 
 import filt_funcs as ff
 
-def get_mean_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', bound=False):
+def get_MV_prof(prof, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', bound=True):
+    
+    '''Takes a profile and a filter scale in meters and returns an array of the filtered signal, squared.
+    
+    ds: xarray dataset with profiles and pressure dimensions
+    lfilter: filter scale in meters
+    variable: coordinate to filter, default=CT
+    dim1: profile dimension, default=N_PROF
+    dim2: pressure dimension, default=PRES_INTERPOLATED'''
+    
+    ds_filt = ff.get_filt_prof(prof, lfilter, variable=variable, dim1=dim1, dim2=dim2)
+    ds_mean_variance = ds_filt**2
+    #ds_mean_variance['TIME'] = xr.DataArray(ds.TIME)
+    
+    return ds_mean_variance
+
+def get_MV(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', bound=True):
     
     '''Takes an xarray and a filter scale in meters and returns an array of the filtered signal, squared.
     
@@ -22,11 +38,12 @@ def get_mean_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTE
     
     ds_filt = ff.get_filt_single(ds, lfilter, variable=variable, dim1=dim1, dim2=dim2, bound=bound)
     ds_mean_variance = ds_filt**2
+    ds_mean_variance['TIME'] = xr.DataArray(ds.TIME)
     
     return ds_mean_variance
 
 
-def get_total_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', bound=False):
+def get_total_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', bound=True):
     
     '''Takes an xarray and a filter scale in meters and returns an array of the squared signal, filtered.
     
@@ -42,7 +59,7 @@ def get_total_variance(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INT
     return ds_total_variance
 
 
-def get_EV(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', bound=False):
+def get_EV(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', bound=True):
     
     '''Takes an xarray and a filter scale in meters and returns an array of the difference between the signal squared then filtered, and the signal filtered then squared. (This quantity we're currently defining as 'EKE')
     
@@ -52,11 +69,11 @@ def get_EV(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', 
     dim1: profile dimension, default=N_PROF
     dim2: pressure dimension, default=PRES_INTERPOLATED'''
     
-    ds_mean_variance = get_mean_variance(ds, lfilter, variable=variable, dim1=dim1, dim2=dim2, bound=bound)
+    ds_mean_variance = get_MV(ds, lfilter, variable=variable, dim1=dim1, dim2=dim2, bound=bound)
     ds_total_variance = get_total_variance(ds, lfilter, variable=variable, dim1=dim1, dim2=dim2, bound=bound)
     ds_eddy_variance = ds_total_variance - ds_mean_variance
     
-    return ds_eddy_variance.mean({'N_PROF'})
+    return ds_eddy_variance
 
 
 def get_NEV(ds,ds_EV,variable,dim1='N_PROF',dim2='PRES_INTERPOLATED',coarsen_scale=40):
@@ -89,3 +106,25 @@ def get_drho_dz (ds, variable, coarsen_scale, dim2='PRES_INTERPOLATED'):
     drho_dz = drho_dz_coarsened.interp(PRES_INTERPOLATED=ds[dim2])
     
     return drho_dz
+
+def get_EKE_da(ds, scales, sample_max, variable):
+    
+    ds = ds.where(ds.sample_rate<sample_max).dropna('N_PROF')
+    ekes_li = []
+    
+    for n in range(0,len(scales)):
+        ekes_li.append(ef.get_EV(ds,scales[n],variable=variable))
+
+    EKES_li = []
+    for n in range(0,len(scales)+1):
+
+        if n==0:
+            EKES_li.append((ekes_li[n]).drop_vars('mask',errors='ignore').drop_vars('N_PROF_NEW',errors='ignore'))
+
+        elif n>0 and n<len(scales):
+            EKES_li.append((ekes_li[n] - ekes_li[n-1]).drop_vars('mask',errors='ignore').drop_vars('N_PROF_NEW',errors='ignore'))
+    
+    EKE_da = xr.concat(EKES_li, dim='EKE')
+    EKE_da = EKE_da.assign_coords(mask=(['N_PROF','PRES_INTERPOLATED'], ekes_li[-1].mask.data))
+    
+    return EKE_da
