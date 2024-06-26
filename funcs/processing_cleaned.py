6dc2edbacb9ae4c3a3919f2f8bc4b6198c51c562
andrewfagerheim <amf2288@columbia.edu>
2024-06-26 13:17:34 +0000

    working on centralized function set

--- funcs/processing_cleaned.py
+++ funcs/processing_cleaned.py
@@ -11,11 +11,30 @@ import argopy
 from argopy import DataFetcher as ArgoDataFetcher
 argo_loader=ArgoDataFetcher(src='gdac',ftp="/swot/SUM05/dbalwada/Argo_sync",progress=True)
 
-def load_argo_box():
+def load_argo_box(float_type, float_ID=None, dim1='N_PROF', dim2='PRES_INTERPOLATED', var_dens='SIG0', var_time='TIME'):
     '''
     inputs:  list of latitude, longitude, depth parameters to load box
     outputs: argo_loader float object
     '''
+
+    float_types = ['argo_float', 'argo_box', 'glider_unprocessed', 'glider_processed']
+    if float_type not in float_types:
+        raise ValueError(f'Invalid float_type value. Please provide one of the following acceptable values {float_types}.')
+        
+    if float_type=='argo_float':
+        pass
+
+    if float_type=='argo_box':
+        pass
+
+    if float_type=='glider_unprocessed':
+        pass
+
+    if float_type=='glider_processed':
+        pass
+
+    return 
+    
 
 def load_argo_float():
     '''
