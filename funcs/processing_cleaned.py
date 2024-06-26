db1fdbaba9df928ad4676532acdf8073bae1186d
andrewfagerheim <amf2288@columbia.edu>
2024-06-26 19:37:29 +0000

    more detailed docstrings

--- funcs/processing_cleaned.py
+++ funcs/processing_cleaned.py
@@ -13,52 +13,75 @@ argo_loader=ArgoDataFetcher(src='gdac',ftp="/swot/SUM05/dbalwada/Argo_sync",prog
 
 #this is my effort to allow for loading all kinds of data (argo loaded by  float ID, argo loaded by box, gliders that haven't been interpolated and ones that have) and return the same kind of data with the same dimensions and variables
 def process_data(float_type, argo_ID=None, argo_box=None, float_unproc=None, float_proc=None, interp_param=None,
-                 dim=['N_PROF','PRES_INTERPOLATED'],
-                 var=['SIG0', 'TIME']):
-    '''
-    inputs:  list of latitude, longitude, depth parameters to load box
-    outputs: argo_loader float object
+                 dims_li=['N_PROF','PRES_INTERPOLATED'],
+                 vars_li=['SIG0']):
+    '''One sentence description
+
+    Longer description that gets into specifics
+
+    Parameters
+    ----------
+    float_type: str
+        One of: argo_float, argo_box, glider_unprocessed, glider_processed
+    argo_ID: int, list
+        Individual float ID or list of IDs
+    argo_box: list, None
+        Of the form: [lon_min, lon_max, lat_min, lat_max, depth_min, depth_max]
+    float_unproc: xr.dataset, None
+        Not-interpolated float data with dimensions: [] and variables: []
+    float_proc: xr.dataset, None
+        Interpolated float data with dimensions: [] and variables []
+    interp_param: xr.dataset, None
+        Parameters for interpolation, of the form: 
+    dims_li: list
+        Names of dataset's dimensions, of the form: [profiles, pressure]
+    vars_li: list
+        Names of dataset's variables, of the form: [density]
+
+    Returns
+    -------
+    xr.dataset
+        Interpolated dataset with TEOS-10 variables.
     '''
 
-    #raising errors if anything doesn't seem right about the inputs
     float_types = ['argo_float', 'argo_box', 'glider_unprocessed', 'glider_processed']
     if float_type not in float_types:
         raise ValueError(f'Invalid float_type value. Please provide one of the following acceptable values {float_types}.')
         
     if float_type=='argo_float':
-        if argo_float is None or interp_param is None:
-            raise TypeError('Must provide a float ID and interpolation parameters for float_type argo_float')
+        if argo_ID is None or interp_param is None:
+            raise TypeError(f'Must provide a float ID and interpolation parameters for float_type argo_float')
         
         ds = load_argo_float(argo_ID)
         print('loading complete')
 
-        ds_interp = get_ds_interp(ds, interp param)
+        ds_interp = get_ds_interp(ds, interp_param)
         print('interpolation complete')
 
     elif float_type=='argo_box':
         if argo_box is None or interp_param is None:
-            raise TypeError('Must provide a float box and interpolation parameters for float_type argo_box')
+            raise TypeError(f'Must provide a float box and interpolation parameters for float_type argo_box')
         
         ds = load_argo_box(argo_box)
         print('loading complete')
 
-        ds_interp = get_ds_interp(ds, interp param)
+        ds_interp = get_ds_interp(ds, interp_param)
         print('interpolation complete')  
 
     elif float_type=='glider_unprocessed':
         if float_unproc is None or interp_param is None:
-            raise TypeError('Must provide an xr ds of the unprocessed glider and interpolation parameters for float_type glider_unprocessed')
+            raise TypeError(f'Must provide an xr ds of the unprocessed glider and interpolation parameters for float_type glider_unprocessed')
 
         ds = float_unproc
         print('loading complete')
 
-        ds_interp = get_ds_interp(ds, interp param)
+        ds_interp = get_ds_interp(ds, interp_param)
         print('interpolation complete')
 
 
     elif float_type == 'glider_processed':
-        if float_unproc is None or interp_param is None:
-            raise TypeError('Must provide an xr ds of the unprocessed glider and interpolation parameters for float_type glider_unprocessed')
+        if float_proc is None:
+            raise TypeError(f'Must provide an xr ds of the processed glider for float_type glider_unprocessed')
 
         ds_interp = float_proc
 
@@ -73,7 +96,19 @@ def process_data(float_type, argo_ID=None, argo_box=None, float_unproc=None, flo
     return ds_interp
 
 def load_argo_float(argo_ID):
-    '''
+    '''One sentence description
+
+    Longer notes
+
+    Parameters
+    ----------
+    argo_ID: int, list
+        Description
+
+    Returns
+    -------
+    xr.dataset
+        Description
     '''
     ds = argo_loader.float(float_ID)
     ds = ds.to_xarray()
@@ -82,7 +117,19 @@ def load_argo_float(argo_ID):
     return ds
 
 def load_argo_box(argo_box):
-    '''
+    '''One sentence description
+
+    Longer notes
+
+    Parameters
+    ----------
+    argo_box: list
+        Description
+
+    Returns
+    -------
+    xr.dataset
+        Description
     '''
 
     ds = argo_loader.region(box)
@@ -94,6 +141,23 @@ def load_argo_box(argo_box):
 #eventually will need to fix this so it works with unprocessed glider data
 #wondering if Dhruv's glider paper already has a function that does this actually
 def get_ds_interp(ds, interp_param):
+    '''One sentence description
+
+    Longer notes
+
+    Parameters
+    ----------
+    ds: xr.dataset
+        Description
+    interp_param: list
+        Description
+
+    Returns
+    -------
+    xr.dataset
+        Description
+    '''
+    
     dp = ds['PRES'].diff('N_LEVELS').sortby('N_PROF')
     ds["sample_rate"] = dp
     ds_interp = ds.argo.interp_std_levels(np.arange(depth_min, depth_max, interp_step))
@@ -103,7 +167,16 @@ def get_ds_interp(ds, interp_param):
     return ds_interp
 
 def add_MLD():
-    '''
+    '''One sentence description
+
+    Longer notes
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    
     '''
 
     MLD_li = []
