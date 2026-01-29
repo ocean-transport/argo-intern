0ee09f7d5072f2d640932ed93a25a377dfaf7a9c
Andrew Fagerheim <amf2288@columbia.edu>
2026-01-29 17:59:21 -0500

    working on plots for pbar_var notebook

--- funcs/filt_funcs.py
+++ funcs/filt_funcs.py
@@ -99,7 +99,7 @@ def get_filt_prof(prof, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERP
     return prof_filt
 
 
-def ds_filt_single(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', bound=True):
+def ds_filt_single(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPOLATED', bound=True, lat='LAT', lon='LON'):
     
     '''Takes an xarray and a filter scale in meters and returns an xarray with additional coordinates N_PRPF_NEW for a sequence that can be plotted and MASK for the boundary correction.
     
@@ -118,11 +118,11 @@ def ds_filt_single(ds, lfilter, variable='CT', dim1='N_PROF', dim2='PRES_INTERPO
     temp[:,:] = filter.gaussian_filter1d(ds[variable], sigma=nfilter, mode='nearest')
     
     ds_filt = xr.DataArray(temp, dims=[dim1, dim2], coords={dim1:ds[dim1], dim2:ds[dim2]})
-    ds_filt = ds_filt.assign_coords(lat=(dim1,ds.lat.data))
-    ds_filt = ds_filt.assign_coords(lon=(dim1,ds.lon.data))
+    ds_filt = ds_filt.assign_coords(LAT=(dim1,ds[lat].data))
+    ds_filt = ds_filt.assign_coords(LON=(dim1,ds[lon].data))
     
-    number=np.arange(0,len(ds_filt[dim1]))
-    ds_filt['N_PROF_NEW']=xr.DataArray(number,dims=ds[dim1].dims)
+    #number=np.arange(0,len(ds_filt[dim1]))
+    #ds_filt['N_PROF_NEW']=xr.DataArray(number,dims=ds[dim1].dims)
     #ds_filt=ds_filt.assign_coords(mask=((dim1,dim2),mask_li))
     
     return ds_filt
